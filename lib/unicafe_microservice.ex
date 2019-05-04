4f3b9a1fab2209257821bacc8d45e1d3684697de
Hugo Holmqvist <hugo.holm@live.com>
2019-05-04 13:57:23 +0300

    Change startup logic

--- lib/unicafe_microservice.ex
+++ lib/unicafe_microservice.ex
@@ -1,16 +1,12 @@
 defmodule UnicafeMicroservice do
-  require UnicafeService
-  require UnicafeRouter
+  use Application
 
-  def init(default_options) do
-    IO.puts "Starting server..."
-    HTTPoison.start
-    default_options
-  end
+  def start(_type, _args) do
+    children = [
+      Plug.Cowboy.child_spec(scheme: :http, plug: UnicafeRouter, options: [port: 4002])
+    ]
 
-  def call(conn, options) do
-    UnicafeRouter.call(conn, UnicafeRouter.init([]))
+    opts = [strategy: :one_for_one, name: UnicafeMicroservice.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
-
-Plug.Adapters.Cowboy.http UnicafeRouter, []

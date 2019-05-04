d4f4c6eb42b3001b094b4d4932cca0273b523cad
Hugo Holmqvist <hugo.holm@live.com>
2019-05-04 14:02:29 +0300

    Add elixir buildpack config, config port

--- lib/unicafe_microservice.ex
+++ lib/unicafe_microservice.ex
@@ -2,8 +2,10 @@ defmodule UnicafeMicroservice do
   use Application
 
   def start(_type, _args) do
+    app_port_s = System.get_env "PORT"
+    app_port = Integer.parse app_port_s
     children = [
-      Plug.Cowboy.child_spec(scheme: :http, plug: UnicafeRouter, options: [port: 4002])
+      Plug.Cowboy.child_spec(scheme: :http, plug: UnicafeRouter, options: [port: app_port])
     ]
 
     opts = [strategy: :one_for_one, name: UnicafeMicroservice.Supervisor]
